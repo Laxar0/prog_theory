49f23c1a63c15904ffdcdb7fbb4f4adc64c11f4b
Kotyk Maksym <maxx.kotik@gmail.com>
2023-11-22 14:25:40 +0200

    Update test.py

--- test.py
+++ test.py
@@ -28,4 +28,7 @@ class TestMyRowsApp(unittest.TestCase):
     def tearDown(self):
         pass
 if __name__ == '__main__':
+    import xmlrunner
+    runner = xmlrunner.XMLTestRunner(output="test-reports")
+    unittest.main(testRunner=runner)
     unittest.main()
